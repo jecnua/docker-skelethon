19356fb1659cb8901a067dd727748497a00289fe
Fabrizio Sabatini <fabrizio.sabatini.it@gmail.com>
2017-05-31 14:22:58 +0100

    Image name now read from env

--- test_travis.sh
+++ test_travis.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # No need to expose any port :)
 
+source ./env.sh
 export GOSS_WAIT_OPTS="-r 90s -s 1s > /dev/null"
 dgoss run \
-  example/example:dev-latest
+  $DOCKER_IMAGE:dev-latest
